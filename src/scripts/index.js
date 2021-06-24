196910f2d37416ac25f64306d71c4c5f37057461
mariagluck <mariamart.berlin@gmail.com>
2021-06-24 16:24:44 +0200

    completed minus styling modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,24 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+//import '../styles/main.scss';
 // Import any additional modules you want to include below \/
 
 
 // \/ All of your javascript should go here \/
 
+const modal = document.querySelector("#myModal");
+const closeModalSpan = document.querySelector(".close");
+  
+const modalPromise = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        resolve();
+        modal.style.display = "none";
+      }, 60);
+});
+
+modalPromise.then(() => {
+    modal.style.display="block"
+ });
+
+closeModalSpan.addEventListener("click",()=>{
+    modal.style.display ="none"
+});
